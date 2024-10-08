856c7669736445397a137312654e57f7197cf98a
Bernhard-Brouwersdam <146329884+Bernhard-Brouwersdam@users.noreply.github.com>
2024-10-08 14:35:45 +0200

    Added <leader>cc in order to exit insert mode via <ESC>

--- lua/bernhard/remap.lua
+++ lua/bernhard/remap.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = " "
+
+-- remap leader pv to open file browser
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- remap leader cc to exit insert-mode
+vim.keymap.set("i", "<leader>cc", "<ESC>")
+
